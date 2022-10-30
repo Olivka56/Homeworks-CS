c679edad2c49cbe32c60eaaab98f713b9fda6647
Vera <olivka130599@gmail.com>
2022-10-30 15:15:22 +0500

    HW1 Task4

---
+++ HW1/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("a= ");
+string a_s = Console.ReadLine();
+int a = int.Parse(a_s);
+
+Console.Write("b= ");
+string b_s = Console.ReadLine();
+int b = int.Parse(b_s);
+
+Console.Write("c= ");
+string c_s = Console.ReadLine();
+int c = int.Parse(c_s);
+
+if (a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.WriteLine(b);
+    }
+    else
+    {
+        Console.WriteLine(c);
+    }
+}
