9bf0fb47602fac232682e53728a6019b82186a7a
Vera <olivka130599@gmail.com>
2022-10-30 14:50:57 +0500

    hw1 task2

---
+++ HW1/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("a= ");
+string a_s = Console.ReadLine();
+int a = int.Parse(a_s);
+
+Console.Write("b= ");
+string b_s = Console.ReadLine();
+int b = int.Parse(b_s);
+
+Console.Write("max = ");
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
