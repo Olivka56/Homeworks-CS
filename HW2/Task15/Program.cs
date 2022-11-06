f2ebb0e24b7c1a20870333b78a98622a6105c916
Vera <olivka130599@gmail.com>
2022-11-06 12:41:43 +0500

    HW2 Task15

---
+++ HW2/Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напишите число: ");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+
+if (n == 6 || n == 7)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
