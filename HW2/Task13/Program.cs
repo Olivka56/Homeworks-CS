5fe26742bf5c5b02817a1fca645de8ffdb5df1d1
Vera <olivka130599@gmail.com>
2022-11-06 12:30:28 +0500

    HW2 Task13

---
+++ HW2/Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Напишите число: ");
+string number = Console.ReadLine();
+int n = int.Parse(number);
+
+if (n / 100 != 0)
+{
+    while (n >= 1000)
+    {
+        n /= 10;
+    }
+    Console.WriteLine(n % 10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
