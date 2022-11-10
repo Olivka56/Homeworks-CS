1485bb5db770908c00c54e95442ed9a09609ba4a
Vera <olivka130599@gmail.com>
2022-11-10 17:55:39 +0600

    HW3 Task19

---
+++ HW3/Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да */
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int n = number;
+int rev = 0;
+while (n > 0)
+{
+    int lastDigit = n % 10;
+    n /= 10;
+    rev = rev * 10 + lastDigit;
+}
+
+Console.WriteLine(number == rev ? "Yes" : "No");
+
