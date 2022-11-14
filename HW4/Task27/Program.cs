ad73950a346a051c4df7585b1953ecd20e1a84e2
Vera <olivka130599@gmail.com>
2022-11-14 22:09:41 +0600

    HW4 Task27

---
+++ HW4/Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+while (number > 0)
+{
+    sum = sum + number % 10;
+    number /= 10;
+}
+
+Console.WriteLine(sum);
