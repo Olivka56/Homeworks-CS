1682fe9ccc1ab0f5bdaf612b9cb0ff4dc90ce265
Vera <olivka130599@gmail.com>
2022-11-17 19:47:07 +0600

    HW5 Task34

---
+++ HW5/Task34/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+Console.Write("Введите длину массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = CreateArray(size);
+PrintArray(array);
+int evens = CountEvens(array);
+
+Console.WriteLine($"Из {size} чисел {evens} четные");
+
+
+int[] CreateArray(int size)
+{
+    int[] numbers = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+
+    return numbers;
+}
+
+int CountEvens(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine("]");
+}
